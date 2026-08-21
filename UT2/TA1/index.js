fb90a39fce8481760580e0aa1d7ea907be512484
Luutyy <lautaramirezlopez@gmail.com>
2026-08-21 13:45:34 -0300

    asa

--- UT2/TA1/index.js
+++ UT2/TA1/index.js
@@ -1,8 +1,16 @@
 function repeatString(text,repeticiones){
+    const resultado = document.getElementById("result")
     for(let i = 0; i < repeticiones; i++){
-        console.log(text);
+        let p = document.createElement("p");
+        p.textContent = text; 
+        resultado.appendChild(p);
     }
 }
+const form = document.getElementById("form");
+form.addEventListener('submit', function(evento) {
+    evento.preventDefault(); // Evita que la página se recargue
 
-repeatString("Hola Mundo", 6)
-repeatString("Hola Mundo", 7)
+    let texto = document.getElementById('texto').value;
+    let numero = document.getElementById('numero').value;
+    repeatString(texto, numero);
+})
--- UT2/TA2/index.js
+++ UT2/TA2/index.js
@@ -3,7 +3,10 @@ function reverseString(text){
     for (let i = text.length - 1; i >= 0 ; i--){
         text2 = text[i] + text2;
     }
+    let text3 = text.split("");
+    let text4 = text3.reverse().join("");
     console.log(text2);
+    console.log(text4);
 }
 
 reverseString("Hola como estas")
